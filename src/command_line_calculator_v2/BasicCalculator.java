a579d9e55229fbe880d19368bf0e151c479f406a
Adam Patterson <awpatterson217@gmail.com>
2017-03-05 02:00:57 -0600

    Update BasicCalculator.java

--- src/command_line_calculator_v2/BasicCalculator.java
+++ src/command_line_calculator_v2/BasicCalculator.java
@@ -3,25 +3,20 @@ package command_line_calculator_v2;
  * The BasicCalculator class provides the user with a title, and calls on 
  * startCalc() and keepGoing() from the Methods class - allowing the user to 
  * execute basic calculator functions.
- */
-
-/*
+ * **********************
  * @author Adam Patterson
+ * **********************
  */
 public class BasicCalculator {
 	static void runBasicCalculator(){
 		System.out.println("********************");
 		System.out.println("* Basic Calculator *");
 		System.out.println("********************\n");
-
 		boolean calcOn = true;
-	
 		// Initializes the program and executes the selected operation on firstNum and nextNum.
 		Methods.startCalc();
-	
 		// This application loops endlessly
 		while(calcOn){
-		
 			// The keepGoing method executes the selected operation 
 			// on currentAnswer and nextNum. (doubles)	  
 			Methods.keepGoing();
