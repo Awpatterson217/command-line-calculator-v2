75436d68c83fa3a3fd93681bddc3689edf981bb7
Adam Patterson <awpatterson217@gmail.com>
2017-03-05 01:56:45 -0600

    Update Methods.java

--- src/command_line_calculator_v2/Methods.java
+++ src/command_line_calculator_v2/Methods.java
@@ -16,75 +16,53 @@ import java.util.Scanner;
  */
 public class Methods {
 	static Scanner userInput = new Scanner(System.in);
-	
 	// startCalc() is the first method executed.
 	static void startCalc(){
-		
 		// An instance of the RunningCalculation class.
 		RunningCalculations runningAnswer = new RunningCalculations();
-		
-		
 		// A simple menu which presents the user with a key.
-		// More operators can be added.
 		System.out.println("Operators");
 		System.out.println("+: Plus");
 		System.out.println("-: Subtract");
 		System.out.println("*: Multiply");
 		System.out.println("/: Divide");
 		System.out.print("\n");
-		
 		// Captures the first number. 
 		System.out.println("First number: ");
 		double num1 = userInput.nextDouble();
 		System.out.print("\n");
-
 		// Captures the operation the user wishes to perform.
 		System.out.println("Operator: ");
 		String firstOperator = userInput.next();
 		System.out.print("\n");
-
 		// Captures the second number.
 		System.out.println("Second number: ");
 		double num2 = userInput.nextDouble();
 		System.out.print("\n");
-		
 		// Executes firstCalc(). This method takes values in the same order captured. 
 		runningAnswer.firstCalc(num1, firstOperator, num2);
 		System.out.print("Current answer: ");
 		System.out.println(runningAnswer.getCurrentAnswer());
 		History.basicHistoryStack.push(runningAnswer.getCurrentAnswer());
-		
-		
-	} // End startCalc().
-	
+	} 
 	// keepGoing() is the second and last method executed.
 	static void keepGoing(){
-	
-		
 		// Creates an instance of the RunningCalculations class.
 		RunningCalculations runningAnswer = new RunningCalculations();
-		
-		// Allows the user to perform another operation on nextNum and currentAnswer.
+		// Perform another operation on nextNum and currentAnswer.
 		System.out.println("Next operator (or 'exit'): ");
 		String nextOperator = userInput.next();
-		
 		if(nextOperator.equalsIgnoreCase("exit"))
 			UI.goCalc();
-		
 		System.out.print("\n");
-
-		// Allows the user to enter the next number.
+		// Enter the next number.
 		System.out.println("Next number: ");
 		double nextNumber = userInput.nextDouble();
 		System.out.print("\n");
-
 		// Performs nextCalc().
 		runningAnswer.nextCalc(nextOperator, nextNumber);
 		System.out.print("Current Answer: ");
 		System.out.println(runningAnswer.getCurrentAnswer());
-		History.basicHistoryStack.push(runningAnswer.getCurrentAnswer());
-		
-	
-	} // End keepGoing().
-	
-} // End Methods class.
+		History.basicHistoryStack.push(runningAnswer.getCurrentAnswer());	
+	} 
+} 
