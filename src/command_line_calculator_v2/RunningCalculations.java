3fad3727232df77f360f66334e430faab8022acc
Adam Patterson <awpatterson217@gmail.com>
2017-03-05 01:58:11 -0600

    Update RunningCalculations.java

--- src/command_line_calculator_v2/RunningCalculations.java
+++ src/command_line_calculator_v2/RunningCalculations.java
@@ -3,67 +3,51 @@ package command_line_calculator_v2;
  * The RunningCalculations class uses a switch statement and
  * performs the requested calculations. This class is
  * instantiated in the Methods class. 
- */
-
-/*
+ * **********************
  * @author Adam Patterson
+ * **********************
  */
 public class RunningCalculations {
 	public static double currentAnswer;
-
 	// Takes in three variables (firstNum, calculation choice, NextNum). 
 	protected void firstCalc(double firstNum, String theOperator, double nextNum){
 		switch(theOperator){
 		case "+":
 			currentAnswer = firstNum + nextNum;
-
 			break; 
 		case "-":
 			currentAnswer = firstNum - nextNum;
-
 			break;
 		case "*":
 			currentAnswer = firstNum * nextNum;
-	
 			break;
 		case "/":
 			if(firstNum > 0){currentAnswer = firstNum / nextNum;}
-			
 			else{System.out.println("ERROR: Division by zero");}
-			
 			break;
 		default:
 			System.out.println("ERROR: Invalid Operator");
 		} 
-	} // End firstCalc.
-	
+	} 
 	// Takes in two variables (calculation choice, nextNum).
 	protected void nextCalc(String theOperator, double nextNum){
 		switch(theOperator){
 		case "+":
 			currentAnswer = currentAnswer + nextNum;
-		
 			break;
 		case "-":
 			currentAnswer = currentAnswer - nextNum;
-
 			break;
 		case "*":
 			currentAnswer = currentAnswer * nextNum;
-			
 			break;
 		case "/":
 			if(currentAnswer > 0){currentAnswer = currentAnswer / nextNum;}
-			
 			else{System.out.println("ERROR: Division by zero");}
-	
 			break;
 		default:
 			System.out.println("ERROR: Invalid Operator");
 		} 
-	} // End nextCalc.
-
-
+	}
 	double getCurrentAnswer() {return currentAnswer;} 
-}// End RunningCalculations class
-
+}
