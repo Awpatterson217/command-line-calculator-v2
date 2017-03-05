1e296e5455ed1c39f991fc3d3f07330bb4c059cd
Adam Patterson <awpatterson217@gmail.com>
2017-03-05 01:53:14 -0600

    Update FinancialFunctions.java

--- src/command_line_calculator_v2/FinancialFunctions.java
+++ src/command_line_calculator_v2/FinancialFunctions.java
@@ -10,7 +10,6 @@ import java.util.*;
 public class FinancialFunctions {
 	static Scanner userInput = new Scanner(System.in);
 	private double value;
-	
 	// Calculates the future value of 'amount'.
 	// Needs additional functionality, like allowing the user to add payments and changing how often interest accrues. 
 	 double futureValue(){
@@ -22,28 +21,22 @@ public class FinancialFunctions {
 		double numOfYears = userInput.nextDouble();
 		value = amount * Math.pow((1+interestRate), numOfYears);
 		return value;
-		
-
-	}// End futureValue()
-	
-	// The presentValue method will allow the user to calculate the present value of some amount
+	}
+	// Calculate the present value of some amount
 	// based upon a stated discount rate.
 	double presentValue(){
 		return value;
-	}// End presentValue()
-	
-	// Will calculate the Present Value of some cash inflows minus the present value of some cash outflows.
+	}
+	// Calculate the Present Value of some cash inflows
+	// minus the present value of some cash outflows.
 	double netPresentValue(){
 		return value;
-	}// End netPresentValue()
-	
-	// The Amortization method will allow a user to calculate loan payments, based on some APR and length of time.
-	// Will provide the user with a table which includes the dollar amount going towards principal and the dollar amount 
-	// towards interest, by payment.
+	}
+	// Calculate loan payments based on some APR and length of time.
+	// Possible amortization table.
 	double Amortization(){
 		return value;
 	}
-	
 	double getValue(){
 		return value;
 	}
