488a0f8b3667c1516f6a72d7234d3a9f34f479ef
Adam Patterson <awpatterson217@gmail.com>
2017-03-05 01:54:27 -0600

    Update Main.java

--- src/command_line_calculator_v2/Main.java
+++ src/command_line_calculator_v2/Main.java
@@ -5,16 +5,12 @@ package command_line_calculator_v2;
  * The BasicCalculator, Methods, and RunningCalculations classes together, make up the basic calculator.
  * 
  * The FinancialFunctions class is instantiated, and contains several financial algorithms. 
- * 
- */
-
-/*
+ * *********************
  * @author Adam Patterson
+ * **********************
  */
 public class Main {
-
 	public static void main(String[] args) {
-			UI.goCalc();
+		UI.goCalc();
 	}
-
 }
