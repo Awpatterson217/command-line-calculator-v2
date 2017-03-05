aac22f4399f19995f977d12442963916ae945bc9
Adam Patterson <awpatterson217@gmail.com>
2017-03-05 01:48:57 -0600

    Removing unnecessary white space.

--- src/command_line_calculator_v2/History.java
+++ src/command_line_calculator_v2/History.java
@@ -8,25 +8,17 @@ import java.util.Stack;
  * @author Adam Patterson
  */
 public class History {
-	
 	// A stack that holds past financial calculations.
 	public static Stack<Double> finHistoryStack = new Stack<Double>();
-	
 	public static void printFinHistory(){
 		if(finHistoryStack.isEmpty()){
 			System.out.println("There is no history.");
 		}else{
 			System.out.printf("OLDEST" + finHistoryStack + "NEWEST");
 			System.out.println("");
-
 		}
 	} // End printFinHistory().
-	// End financial calculations stack.
-	
-	
-	// A stack that holds past basic calculations.
 	public static Stack<Double> basicHistoryStack = new Stack<Double>();
-	
 	public static void printBasicHistory(){
 		if(basicHistoryStack.isEmpty()){
 			System.out.println("There is no history.");
@@ -35,5 +27,4 @@ public class History {
 			System.out.println("");
 		}
 	}// printBasicHistory().
-	// End basic calculations stack.
 }
